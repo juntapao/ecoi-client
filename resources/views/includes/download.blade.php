b262e646277be34a60f0589f47e51a02cf1e3eaa
Jun Tapao <juntapao@MAA>
2022-11-03 15:30:20 +0800

    fixed error in generating report

--- resources/views/includes/download.blade.php
+++ resources/views/includes/download.blade.php
@@ -1,5 +1,5 @@
 @if(session('download'))
     <?php if(strpos(url()->current(), 'public') !== false) $public = '../'; else $public = '';  ?>
-    <meta http-equiv="refresh" content="2; url=../storage/downloads/{{ session('download') }}">
+    <meta http-equiv="refresh" content="2; url=../storage/{{ session('download') }}">
     {{ session()->forget('download') }}
 @endif
