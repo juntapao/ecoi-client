c20a48eb8cc3bb68c7ac9c4af3307b126a534d71
John Patrick S. Bendana <JPBendana@MAA>
2023-12-20 14:39:53 +0800

    response included in laravel console command ReSyncTransactions

--- app/Console/Commands/ReSyncTransactions.php
+++ app/Console/Commands/ReSyncTransactions.php
@@ -19,20 +19,33 @@ class ReSyncTransactions extends Command
 
     public function handle()
     {
-        $body = [
-            'terminal_signature' => Setting::find(1)->value
-        ];
+        try {
+            //code...
+            $body = [
+                'terminal_signature' => Setting::find(1)->value
+            ];
 
-        $server = CommonController::curl(config('app.ecoi_server_url').'/api/cancelled-transactions', 'post', $body);
+            $server = CommonController::curl(config('app.ecoi_server_url').'/api/cancelled-transactions', 'post', $body);
 
-        if(isset($server->status_code)){
-            if($server->status_code == 200 && isset($server->data)) {
-                Transaction::whereIn('coi_number', $server->data)
-                    ->where('status', '!=', 'deleted')
-                    ->update([
-                        'uploaded' => false,
-                    ]);
+            if(isset($server->status_code)){
+                if($server->status_code == 200 && isset($server->data)) {
+                    $update = Transaction::whereIn('coi_number', $server->data)
+                        ->where('status', '!=', 'deleted')
+                        ->update([
+                            'uploaded' => false,
+                        ]);
+                }
             }
+
+            if($update)
+                $this->info('Command executed successfully.');
+            else
+                $this->info('No cancelled transaction to be corrected.');
+            return 0; // Success
+        } catch (\Throwable $th) {
+            //throw $th;
+            $this->error('An error occurred: ' . $th->getMessage());
+            return 1; // Failure
         }
     }
 }
